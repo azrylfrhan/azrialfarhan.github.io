9178e489678a20fbc8e6f69bbf7a97782adb9a79
azrylfrhan <azrialfar@gmail.com>
2024-01-02 16:00:27 +0700

    portfolioazrial

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#fffffe',
+        second: '#f9bc60',
+        paragraph: '#abd1c6',
+        buttontext: '#001e1d',
+        aboutcolor: '#0f3433',
+        homecolor : '#004643',
+        aboutbg : '#abd1c6',
+        main: '#e8e4e6',
+      },
+      screens:{
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
+
